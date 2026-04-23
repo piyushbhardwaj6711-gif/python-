83450da9881a3ad8e4dcbe404c44079d5d82d154
piyush bhardwaj <piyushbhardwaj6711@gmail.com>
2026-04-23 23:16:50 +0530

    Added Python basics file

---
+++ python first.py
@@ -0,0 +1,52 @@
+# # -----------------------------------
+# Python Basics Practice
+# Author: Piyush Bhardwaj
+# -----------------------------------
+
+# Print Statement
+print("Hello")
+
+# Variables
+name = "Piyush"
+age = 23
+
+print("Name:", name)
+print("Age:", age)
+
+# Data Types
+print("Type of age:", type(age))
+print("Type of name:", type(name))
+
+# Addition Example
+a = 23
+b = 34
+sum_result = a + b
+
+print("Sum =", sum_result)
+
+# -----------------------------------
+# Type Casting
+# Changing one data type into another
+# -----------------------------------
+
+a = int("3")
+b = 3
+
+sum_result = a + b
+print("Sum after type casting =", sum_result)
+
+# Another Example
+x = 23
+y = int("56")
+
+print("Sum =", x + y)
+
+# Float + Integer
+num1 = 2
+num2 = 2.4
+
+print("Total =", num1 + num2)
+
+# User Input
+user_age = input("Enter your age: ")
+print("Your age is:", user_age)
